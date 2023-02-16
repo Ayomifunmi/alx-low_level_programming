d95fd3bf65c8665b494fc11f593856d253128ade
Ayomiposi Falade <ayomiposif.falade@gmail.com>
2023-02-16 06:03:12 -0800

    Sixth Ctask

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Printing a program with the printf function
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
